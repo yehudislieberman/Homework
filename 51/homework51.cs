bcfa3097a8171ceddcd0b9710a7ce94d3ff5df6c
yehudislieberman <yehudislieberman@gmail.com>
2023-05-31 18:48:42 -0400

    Homework 51

---
+++ 51/homework51.cs
@@ -0,0 +1,27 @@
+﻿namespace Homework_51 {
+    public class Shirt {
+        public string Color { get; set; }
+        public string Pattern { get; set; }
+
+        public Shirt(string color, string pattern)
+        {
+            Color = color;
+            Pattern = pattern;
+        }
+    }
+    public class Program {
+        static void Main(string[] args)
+        {
+            string[] colors = { "red", "green", "blue" };
+            string[] patterns = { "striped", "checked", "plain" };
+            foreach (string color in colors)
+            {
+                foreach (string pattern in patterns)
+                {
+                    Shirt shirt = new Shirt(color, pattern);
+                    Console.WriteLine($"{shirt.Color}: {shirt.Pattern}");
+                }
+            }
+        }
+    }
+}
